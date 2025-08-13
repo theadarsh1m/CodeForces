0377d8392d3ff50239c5819cd9dffa3d42b24bea
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-08-13 23:20:30 +0530

    edited beutiull mat

--- Problem Set/BeautifulMatrix.java
+++ Problem Set/BeautifulMatrix.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 public class BeautifulMatrix {
     public static void main(String[] args) {
-        int[][] mat = new int[5][5];
         Scanner sc = new Scanner(System.in);
 
         int count = 0;
 
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 5; j++) {
-                mat[i][j] = sc.nextInt();
-                if(mat[i][j] == 1) {
+                if(sc.nextInt() == 1) {
                     count = Math.abs(i-2) + Math.abs(2 - j);
                     break;
                 }
